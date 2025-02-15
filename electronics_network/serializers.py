61365415d4f9018d6b779952c0244a1bf55ee546
SergeyKotkov <161629099+SergeyKotkov@users.noreply.github.com>
2025-02-15 21:37:51 +0300

    создание основных моментов для работы приложения

---
+++ electronics_network/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import NetworkNode
+
+class NetworkNodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NetworkNode
+        fields = '__all__'
+        read_only_fields = ('debt',)
